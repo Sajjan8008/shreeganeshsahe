cf4232237501bccd9d17ad36642e123f01213640
ksajjan8008 <ksajjan8008@gmail.com>
2023-10-28 09:27:27 +0530

    setup packege

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
-const path = require('path');
+const {path,join} = require('path');
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(join(__dirname, 'build')));
+// app.use('/public', express.static(join(_dirname, '../public')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(join(__dirname, 'build', 'index.html'));
 });
 
 
-app.listen(9000);
+
+
+
+// Use Routes
+// app.use('/', routes);
+
+// Serve static assets if in production
+// if (isProduction) {
+  // Set static folder
+  app.use(express.static(join(__dirname, '../../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(resolve(_dirname, '../..', 'client', 'build', 'index.html')); // index is in /server/src so 2 folders up
+  });
+// }
+
+const port = process.env.PORT || 5000;
+  app.listen(port, () => console.log(`Server started on port ${port}`));
