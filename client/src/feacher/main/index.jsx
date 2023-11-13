73267db569b37c1c9a87c76d46ce70d359562898
ksajjan8008 <ksajjan8008@gmail.com>
2023-11-13 17:10:10 +0530

    same change

--- client/src/feacher/main/index.jsx
+++ client/src/feacher/main/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Home from '../home';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import './index.css'
 function Main() {
 
@@ -14,10 +14,10 @@ function Main() {
   return (
     <>
       <Home />
-      <Button variant="outline-info" size="lg"
+      {/* <Button variant="outline-info" size="lg"
         onClick={routeChange}
        >Continue
-      </Button>
+      </Button> */}
     </>
   )
 }
