823ab2f2cff70b8d9cc2081238b889cb8ff35614
ksajjan8008 <ksajjan8008@gmail.com>
2023-11-13 17:27:20 +0530

    same change

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,21 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Navigate
+// } from "react-router-dom";
 
-import About from './feacher/about/about';
+// import About from './feacher/about/about';
 // import Contact from '../src/feacher/contact/contact';
-import Home from './feacher/homepage/homepage';
+// import Home from './feacher/homepage/homepage';
 import Main from './feacher/main/index';
 
 function App() {
 
   return (
     <div className="app">
-
-    <Router >
-        <Routes>
-
-        <Route
-            exact
-            path="/"
-            element={<Main />
-          
-          }
-          />
-
-          <Route
-            exact
-            path="/sajjan"
-            element={<Home />}
-          />
-
-
-          <Route
-            path="/about"
-            element={<About/>}
-          />
-
-
-          <Route
-            path="*"
-            element={<Navigate to="/" />}
-          />
-        </Routes>
-      </Router>
+<Main/>
 
     </div>
   );
