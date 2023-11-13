8aee7b0a51226131a1560b4e56b78a1273638b79
ksajjan8008 <ksajjan8008@gmail.com>
2023-11-13 17:28:44 +0530

    same change

--- client/src/feacher/main/index.jsx
+++ client/src/feacher/main/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import Home from '../home';
 // import Button from 'react-bootstrap/Button';
 import './index.css'
 function Main() {
 
-  let navigate = useNavigate();
-  const routeChange = () => {
-    let path = `/sajjan`;
-    navigate(path);
-  }
+  // let navigate = useNavigate();
+  // const routeChange = () => {
+  //   let path = `/sajjan`;
+  //   navigate(path);
+  // }
 
   return (
     <>
